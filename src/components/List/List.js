f943032c2d3937e049f6a641b48f342193979f9e
Ewelina Słyk <ewelinaslyk@wp.pl>
2020-06-22 01:49:20 +0200

    Add src/components/List/List.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,23 +3,27 @@ import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
 import Column from '../Column/Column.js';
+import {settings} from '../../data/dataStore';
+import ReactHtmlParser from 'react-html-parser';
 
 class List extends React.Component {
   static propTypes = {
     title: PropTypes.node.isRequired,
-    children: PropTypes.node.isRequired,
-    imageSrc: PropTypes.string,
+    description: PropTypes.node,
+    columns: PropTypes.array,
+    image: PropTypes.node,
   }
 
   static defaultProps = {
-    children: <p>I can do all the things!!!</p>,
+    description: settings.defaultListDescription,
   }
 
   render() {
+    const { title, image, description, columns, addColumn } = this.props;
     return (
       <section className={styles.component}>
-         <Hero titleText={this.props.title} imageSrc={this.props.heroImageSrc}/>
-        <div className={styles.description}>{this.props.children}</div>
+         <Hero titleText={this.props.title} image={this.props.heroImage}/>
+         <div className={styles.description}>{ReactHtmlParser(description)}</div>
         <div className={styles.columns}>
           <Column title="Animals"/>
           <Column title="Plants"/>
