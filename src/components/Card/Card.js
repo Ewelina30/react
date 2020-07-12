a281398de28e2dbb57544e4fc4bd6b3d3786d81e
Ewelina Słyk <ewelinaslyk@wp.pl>
2020-07-12 20:52:25 +0200

    Add src/components/Card/Card.js

---
+++ src/components/Card/Card.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import styles from './Card.scss';
+import PropTypes from 'prop-types';
+import { Draggable } from 'react-beautiful-dnd';
+
+
+class Card extends React.Component {
+    static propTypes = {
+      title: PropTypes.node.isRequired,
+      id: PropTypes.string,
+      index: PropTypes.number,
+    };
+
+    render() {
+         const { title, id, index } = this.props;
+           return (
+             <Draggable draggableId={id} index={index}>
+               {(provided) => (
+                 <article
+                   className={styles.component}
+                   {...provided.draggableProps}
+                   {...provided.dragHandleProps}
+                   ref={provided.innerRef}
+                 >
+                   {title}
+                 </article>
+               )}
+             </Draggable>
+           );
+         }
+      
+        render() {
+          const {title} = this.props;
+          return (
+            <section className={styles.component}>
+              <h3>{title}</h3>
+            </section>
+          );
+        
+        }
+      }
+      
+      export default Card;
