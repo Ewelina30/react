0be165ba6efc869843b3f8441acf2ac04f38734a
Ewelina Słyk <ewelinaslyk@wp.pl>
2020-07-13 19:08:27 +0200

    Add src/components/Column/Column.js

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,20 +1,57 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
+import Card from '../Card/Card.js';
+import Creator from '../Creator/Creator.js';
+import { settings } from '../../data/dataStore';
+import Icon from '../Icon/Icon.js';
+import { Droppable } from 'react-beautiful-dnd';
 
 class Column extends React.Component {
-    static propTypes = {
-        title: PropTypes.node.isRequired,
-    };
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+    cards: PropTypes.array,
+    icon: PropTypes.node,
+    addCard: PropTypes.func,
+    id: PropTypes.string,
+  };
 
-        render() {
-            return(
-                <section className={styles.component}>
-                    <h3 className={styles.title}>{this.props.title}</h3>
-                </section>
-            );
-        }
-    }
+  static defaultProps = {
+    icon: settings.defaultColumnIcon,
+  };
 
-
+  render() {
+    const { title, icon, cards, addCard } = this.props;
+    return (
+      <section className={styles.component}>
+        <h3 className={styles.title}>
+          <span className={styles.icon}>
+            <Icon name={icon} />
+          </span>
+          {title}
+        </h3>
+        {cards.map((cardData) => (
+          <Card key={cardData.id} {...cardData} />
+        ))}
+        {<Droppable droppableId={id}>
+        {(provided) => (
+          <div
+            className={styles.cards}
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {cards.map((cardData) => (
+              <Card key={cardData.id} {...cardData} />
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+        </Droppable> }
+        <div>
+          <Creator text={settings.cardCreatorText} action={addCard} />
+        </div>
+      </section>
+    );
+  }
+}
 export default Column;
